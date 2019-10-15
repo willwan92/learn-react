1c9664fc21f587d4bb63712c8ddee46180d2cd43
wanchong <wilson3cy@163.com>
2019-10-15 14:33:34 +0800

    react 哲学实践

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import CommentList from './commont-box/CommentList';
-import CommentForm from './commont-box/CommentForm';
-import './commont-box/CommentBox.css';
+
+import FilterableProductTable from './thinking-in-react/filterableProductTable';
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+// import CommentList from './commont-box/CommentList';
+// import CommentForm from './commont-box/CommentForm';
+// import './commont-box/CommentBox.css';
 
 // import Clock from './clock/clock.js';
 // import './clock/clock.css';
@@ -9,22 +21,26 @@ import './commont-box/CommentBox.css';
 // import SnapShotSample from './snapshot-sample/snapshotSample.js';
 // import './snapshot-sample/snapshotSample.css';
 
-const comments = [
-  {
-    author: "Nate",
-    content: "Hello React! This is a sample comment.",
-  },
-  { author: "Kevin", content: "Hello Redux!" },
-  { author: "Bood", content: "Hello Rekit!" },
-];
+// const comments = [
+//   {
+//     author: "Nate",
+//     content: "Hello React! This is a sample comment.",
+//   },
+//   { author: "Kevin", content: "Hello Redux!" },
+//   { author: "Bood", content: "Hello Rekit!" },
+// ];
 
 function App() {
   return (
-    <div className="comment-box">
-      <h1>Comments(3)</h1>
-      <CommentList comments={comments} />
-      <CommentForm />
+		<div className="App">
+			<FilterableProductTable products={PRODUCTS} />
 		</div>
+
+    // <div className="comment-box">
+    //   <h1>Comments(3)</h1>
+    //   <CommentList comments={comments} />
+    //   <CommentForm />
+		// </div>
 		
 		// <div className="clock-box">
 		// 	<Clock />
