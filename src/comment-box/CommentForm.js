84ae2639acfe94e0497c15c0a40b348605235b7a
wanchong <wilson3cy@163.com>
2019-11-09 13:12:00 +0800

    高阶组件实践

--- src/comment-box/CommentForm.js
+++ src/comment-box/CommentForm.js
@@ -1,14 +1,19 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-export default class CommentForm extends PureComponent {
+import withTimer from '../higher-order-components/withTimer'
+
+export class CommentForm extends PureComponent {
 
     render() {
         return (
             <form className="comment-form">
                 <textarea></textarea>
+								<p>{this.props.time.toLocaleString()}</p>
                 <button>submit</button>
             </form>
         )
     }
 }
+
+export default withTimer(CommentForm)
--- src/higher-order-components/withTImer.js
+++ src/higher-order-components/withTImer.js
@@ -1,26 +1,27 @@
-import React  from 'react';
+import React from 'react';
 
 export default function withTimer(WrappedComponent) {
-    return class extends React.Component {
-        state = { time: new Date() };
+  return class extends React.Component {
+    state = { time: new Date() };
 
-        componentDidMount() {
-            this.timerId = setInterval(() => this.tick(), 1000);
-        }
+    componentDidMount() {
+      this.timerId = setInterval(() => this.tick(), 1000);
+    }
 
-        componentWillUnmount() {
-            clearInterval(this.timerId);
-        }
+    componentWillUnmount() {
+      clearInterval(this.timerId);
+    }
 
-        tick() {
-            this.setState({
-                time: new Date()
-            })
-        }
+    tick() {
+      this.setState({
+        time: new Date()
+      })
+    }
 
-        render() {
-            return <WrappedComponent item={this.state.time}  {...this.props} />
-        }
+    render() {
+      return <WrappedComponent time = { this.state.time } {...this.props }
+      />
+    }
 
-    };
+  };
 }
